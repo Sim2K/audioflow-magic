6c89368e4af29b8c6feb09cb6459792ba7680d4d
Sim2K <sim2kuk@gmail.com>
2025-01-29 17:13:03 +0000

    iOS Audio test alac

--- src/utils/audioTypes.ts
+++ src/utils/audioTypes.ts
@@ -11,7 +11,7 @@ export const mimeTypes = {
   webm: ['audio/webm;codecs=opus', 'audio/webm'],
   mp4: [
     //'audio/mp4;codecs=mp4a.40.2',
-    'audio/mp4;codecs=aac',
+    //'audio/mp4;codecs=aac',
     'audio/mp4;codecs=alac',
     'audio/aac',
     'audio/mp4'
@@ -28,7 +28,7 @@ export const formatConfigs = {
   },
   mp4: {
     mimeType: 'audio/mp4',
-    codec: 'aac', //'mp4a.40.2',  // Standard AAC codec identifier
+    codec: 'alac', //'mp4a.40.2',  // Standard AAC codec identifier
     extension: 'm4a',
     bitrate: 32000,
     sampleRate: 22050
